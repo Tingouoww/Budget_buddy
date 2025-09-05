79a60b60ff80bc446047b3370bea3d12dbf465fe
Tingouoww <tingouo20@gmail.com>
2025-09-05 13:48:57 +0800

    TransactionDAOImpl v1

--- src/main/java/com/ting/dao/TransactionDAOImpl.java
+++ src/main/java/com/ting/dao/TransactionDAOImpl.java
@@ -1,3 +1,35 @@
 package com.ting.dao;
 
 import com.ting.model.Transaction;
+import com.ting.dao.TransactionDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TransactionDAOImpl implements TransactionDAO{
+    @Override
+    public void addTransaction(Transaction transaction) {
+        System.out.println("TransactionDAOImpl.addTransaction");
+    }
+
+    @Override
+    public List<Transaction> getAllTransaction() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Transaction getTransactionById(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateTransaction(Transaction transaction) {
+        return;
+    }
+
+    @Override
+    public void deleteTransaction(int id) {
+        return;
+    }
+}
